4afb37f0bb2a031b567ec037326894bab68d460f
TiagoMontes <tiagosantos6303@gmail.com>
2025-03-05 14:58:58 -0500

    fix jest config

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,15 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} **/
+/** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
+  preset: "ts-jest",
   testEnvironment: "node",
+  testMatch: ["**/src/tests/**/*.test.ts"],
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
   transform: {
-    "^.+.tsx?$": ["ts-jest",{}],
+    "^.+\\.ts?$": ["ts-jest", {}],
   },
-}
+  roots: ["<rootDir>/src/tests"],
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  clearMocks: true,
+  testPathIgnorePatterns: ["<rootDir>/dist/"],
+};
